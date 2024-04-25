42311a9aa8e338dbb0a1aea3c1ad4c9b9d5dbecf
Banerjee Aditya <aditya.banerjee@activate.sg>
2024-04-25 12:03:59 +0800

    add process message handler for bot

--- bot/src/components/client.ts
+++ bot/src/components/client.ts
@@ -1,13 +1,20 @@
-import Client from "node-telegram-bot-api";
+import Client, { Message } from "node-telegram-bot-api";
 import { polling } from "constants/polling";
 
 const { BOT_TOKEN = "DUMMY-TOKEN", WEBHOOK_URL = "DUMMY_URL" } = process.env;
 
+export const processMessage = async (message: Message) => {
+    const { chat, text } = message;
+    return { chat, text: `Message received: ${text}` };
+};
+
 export const getClient = () => {
     const client = new Client(BOT_TOKEN, { polling });
     if (!polling) client.setWebHook(`${WEBHOOK_URL}${BOT_TOKEN}`);
-    client.on("message", ({ chat, text }) => {
-        client.sendMessage(chat.id, `Message received: ${text}`);
+    client.on("message", async (message: Message) => {
+        const reply = await processMessage(message);
+        const { chat, text } = reply;
+        client.sendMessage(chat.id, text);
     });
     return client;
 };
